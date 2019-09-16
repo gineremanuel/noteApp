78cbcfbac9753329a36d43081a23bca25dfa1850
Emanuel Giner <emanuelginer77@gmail.com>
2019-09-16 16:39:33 -0300

    Some fix

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule ],
-    exports: [MatButtonModule, MatIconModule ],
+    imports: [MatButtonModule, MatIconModule, MatCardModule, MatGridListModule, MatToolbarModule ],
+    exports: [MatButtonModule, MatIconModule, MatCardModule, MatGridListModule, MatToolbarModule ],
 })
 
 export class MaterialModule { }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Reminder } from '../reminder.model';
-import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-notes',
